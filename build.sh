74ed6d73c8b19a5bcf657b8cf5173c12eaca71d9
Stefano Stabellini <sstabellini@kernel.org>
2017-05-25 14:15:02 -0700

    Initial commit
    Signed-off-by: Stefano Stabellini <stefano@aporeto.com>

---
+++ build.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Copyright 2017 CoreOS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+set -o pipefail
+set -o nounset
+
+# Generate the aci image
+if [[ -z "$GOPATH" ]]
+then
+    echo Please set you GOPATH correctly
+    exit 1
+fi
+
+execs="enter run stop"
+
+# Clean the repo, but save the vendor area
+if [ "x${1:-}" != "x" ] && [ "clean" == "$1" ]; then
+    echo "cleaning project"
+    rm -rf kernel/out
+    rm -rf kernel/build
+    rm -rf target
+    rm -f stage1-xen.aci
+    rm -f aci/actool
+
+    exit 0
+fi
+
+# Build up the target directory and the rootfs
+if [ ! -d target ]; then
+    mkdir -p target/rootfs
+    mkdir -p target/rootfs/opt/stage2
+    mkdir -p target/rootfs/rkt/status
+    cd target/rootfs && ln -s flavor xen && cd ../..
+fi
+
+for i in $execs; do
+    cp files/$i target/rootfs
+done
+
+# Build the kernel and initrd
+kernel/make-kernel
+cp kernel/out/kernel target/rootfs
+kernel/make-initrd
+cp kernel/out/initrd target/rootfs
+
+cp aci/aci-manifest.in target/manifest
+
+if [ -f stage1-xen.aci ]; then
+    rm stage1-xen.aci
+fi
+
+# Build actool
+go get github.com/appc/spec/actool
+go build -o ./aci/actool github.com/appc/spec/actool
+
+./aci/actool build target stage1-xen.aci
